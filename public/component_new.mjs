512945a3b68dc0c258589aab448e4d23182d8737
JaredMathis <jared.mathis@native.consulting>
2023-01-17 18:44:21 -0500

    Tue Jan 17 2023 18:44:21 GMT-0500 (Eastern Standard Time)

--- public/component_new.mjs
+++ public/component_new.mjs
@@ -2,7 +2,7 @@ import { button_back } from "./button_back.mjs";
 import { element_input } from "./element_input.mjs";
 
 export function component_new(parent, view) {
-    let name = element_input(parent);
+    let name = element_input(parent, 'Name of Game');
     name.focus();
     button_back(parent, view);
 }
--- public/element_input.mjs
+++ public/element_input.mjs
@@ -1,8 +1,9 @@
 import { element } from "./element.mjs";
 import { element_style_container } from "./element_style_container.mjs";
 
-export function element_input(parent) {
+export function element_input(parent, placeholder) {
     let input = element(parent, 'input');
+    input.placeholder = placeholder;
     element_style_container(input);
     input.style.padding = '0.7vh';
     return input;
