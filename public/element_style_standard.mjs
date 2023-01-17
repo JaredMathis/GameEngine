f54508b68c0bd635c6e8f3189c2b79695a828917
JaredMathis <jared.mathis@native.consulting>
2023-01-17 17:16:29 -0500

    Tue Jan 17 2023 17:16:29 GMT-0500 (Eastern Standard Time)

--- public/element_style_standard.mjs
+++ public/element_style_standard.mjs
@@ -4,4 +4,11 @@ import { element_style_font_size_standard } from "./element_style_font_size_stan
 export function element_style_standard(e) {
     element_style_font_size_standard(e);
     element_style_border_rounded(e);
+    let value = '0.3vh';
+    element_style_margin_y(e, value);
 }
+function element_style_margin_y(e, value) {
+    e.style.marginTop = value;
+    e.style.marginBottom = value;
+}
+
