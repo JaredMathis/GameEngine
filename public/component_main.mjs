6832689233d6510ab530c5318e60f1c06ba5f24b
JaredMathis <jared.mathis@native.consulting>
2023-01-17 17:27:56 -0500

    Tue Jan 17 2023 17:27:56 GMT-0500 (Eastern Standard Time)

--- public/component_main.mjs
+++ public/component_main.mjs
@@ -1,12 +1,13 @@
 import { element_button_standard } from "./element_button_standard.mjs";
 import { element_style_background_color_standard } from "./element_style_background_color_standard.mjs";
+import { noop } from "./noop.mjs";
 
 export function component_main(parent) {
     element_style_background_color_standard(parent);
     element_button_standard(parent, 'New', noop);
     element_button_standard(parent, 'Open', noop);
     element_button_standard(parent, 'Settings', () => {
-        
+
     });
 }
 
