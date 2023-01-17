1f60c506426c02e42cf411c2fdcacdd43226c2d1
JaredMathis <jared.mathis@native.consulting>
2023-01-17 18:21:07 -0500

    Tue Jan 17 2023 18:21:06 GMT-0500 (Eastern Standard Time)

--- public/view_create.mjs
+++ public/view_create.mjs
@@ -4,6 +4,7 @@ import { element_div } from "./element_div.mjs";
 export function view_create(parent) {
     let container = element_div(parent);
     let child;
+    let stack = [];
     let most_recent;
     let result = {
         set: function view_set(create) {
@@ -13,11 +14,22 @@ export function view_create(parent) {
             }
             child = element_div(container);
             create(child);
+            stack.push(create);
         },
         refresh: function view_refresh() {
             result.set(most_recent);
+        },
+        pop: function view_stack(create) {
+            stack.pop();
+            if (stack.length) {
+                result.view_set(list_last(stack));
+            }
         }
     };
     return result
+}
+
+function list_last(list) {
+    return list[list.length - 1];
 }
 
