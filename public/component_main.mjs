6746832882ef6ed7ea70720ae422894eb10e2a5a
JaredMathis <jared.mathis@native.consulting>
2023-01-17 17:14:47 -0500

    Tue Jan 17 2023 17:14:47 GMT-0500 (Eastern Standard Time)

--- public/component_main.mjs
+++ public/component_main.mjs
@@ -1,11 +1,10 @@
-import { element_button_width_full } from "./element_button_width_full.mjs";
-import { element_style_standard } from "./element_style_standard.mjs";
+import { element_button_standard } from "./element_button_standard.mjs";
 
 export function component_main(parent) {
     const button_text = 'New';
-    let e = element_button_width_full(parent, button_text);
-    element_style_standard(e);
+    element_button_standard(parent, button_text);
 }
+
 
 
 
---
+++ public/element_button_standard.mjs
@@ -0,0 +1,7 @@
+import { element_button_width_full } from "./element_button_width_full.mjs";
+import { element_style_standard } from "./element_style_standard.mjs";
+
+export function element_button_standard(parent, button_text) {
+    let e = element_button_width_full(parent, button_text);
+    element_style_standard(e);
+}
