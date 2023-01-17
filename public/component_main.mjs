6a1f1b45f3936c5ac643fa6cf471c28bc3ee631b
JaredMathis <jared.mathis@native.consulting>
2023-01-17 17:08:37 -0500

    Tue Jan 17 2023 17:08:37 GMT-0500 (Eastern Standard Time)

--- public/component_main.mjs
+++ public/component_main.mjs
@@ -1,13 +1,8 @@
-import { element_button } from "./element_button.mjs";
-import { element_style_width_full } from "./element_style_width_full.mjs";
+import { element_button_width_full } from "./element_button_width_full.mjs";
 
 export function component_main(parent) {
     const button_text = 'New';
     element_button_width_full(parent, button_text);
 }
 
-function element_button_width_full(parent, button_text) {
-    let e = element_button(parent, button_text);
-    element_style_width_full(e);
-}
 
---
+++ public/element_button_width_full.mjs
@@ -0,0 +1,7 @@
+import { element_button } from "./element_button.mjs";
+import { element_style_width_full } from "./element_style_width_full.mjs";
+
+export function element_button_width_full(parent, button_text) {
+    let e = element_button(parent, button_text);
+    element_style_width_full(e);
+}
