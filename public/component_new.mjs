21ca8923085916c210b1800b25d8baed998012dd
JaredMathis <jared.mathis@native.consulting>
2023-01-17 18:48:26 -0500

    Tue Jan 17 2023 18:48:26 GMT-0500 (Eastern Standard Time)

--- public/component_new.mjs
+++ public/component_new.mjs
@@ -1,8 +1,19 @@
 import { button_back } from "./button_back.mjs";
+import { element_button_standard } from "./element_button_standard.mjs";
 import { element_input } from "./element_input.mjs";
+import { game_prefix } from "./game_prefix.mjs";
+import { local_storage_set } from "./local_storage_set.mjs";
 
 export function component_new(parent, view) {
     let name = element_input(parent, 'Name of Game');
     name.focus();
+    element_button_standard(parent, 'Create', () => {
+        local_storage_set(() => game_prefix() + name.value, current => {
+            current = current || { name: name.value }
+            return current;
+        })
+        view.pop();
+    });
     button_back(parent, view);
 }
+
---
+++ public/game_prefix.mjs
@@ -0,0 +1,3 @@
+export function game_prefix() {
+    return 'game_';
+}
