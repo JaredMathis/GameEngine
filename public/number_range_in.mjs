c7fe8ff46ab6a152a7791841423620d1e6bd8ca0
JaredMathis <jared.mathis@native.consulting>
2023-01-17 18:05:22 -0500

    Tue Jan 17 2023 18:05:22 GMT-0500 (Eastern Standard Time)

--- public/number_range_in.mjs
+++ public/number_range_in.mjs
@@ -1,8 +1,8 @@
 import { number_max } from "./number_max.mjs";
 import { number_min } from "./number_min.mjs";
 
-export function number_range_in(current) {
-    current = number_max(current, 5);
-    current = number_min(current, 0.5);
+export function number_range_in(current, min_value, max_value) {
+    current = number_max(current, max_value);
+    current = number_min(current, min_value);
     return current;
 }
