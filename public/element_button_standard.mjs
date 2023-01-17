f9709d11909a2c90b829a656bc619b3ea6ea76e0
JaredMathis <jared.mathis@native.consulting>
2023-01-17 17:23:44 -0500

    Tue Jan 17 2023 17:23:44 GMT-0500 (Eastern Standard Time)

--- public/element_button_standard.mjs
+++ public/element_button_standard.mjs
@@ -6,7 +6,11 @@ export function element_button_standard(parent, button_text) {
     let e = element_button_width_full(parent, button_text);
     element_style_standard(e);
     e.style.color = color_white();
-    e.style.border = `0.3vh solid #00F`;
+    e.style.border = `0.3vh solid ${color_blue_light()}`;
     e.style.background = `#00C`;
+}
+
+function color_blue_light() {
+    return `#00F`
 }
 
