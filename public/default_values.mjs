396a3b7dfbb4969347fe5f00086c4aaee5a01e15
JaredMathis <jared.mathis@native.consulting>
2023-01-17 18:59:57 -0500

    Tue Jan 17 2023 18:59:57 GMT-0500 (Eastern Standard Time)

--- public/default_values.mjs
+++ public/default_values.mjs
@@ -2,9 +2,12 @@ import { values_for_each } from "./values_for_each.mjs";
 
 export function default_values(object, defaults) {
     values_for_each(defaults, (value, key) => {
-        if (!object[key]) {
+        if (!object.hasOwnProperty(key)) {
             object[key] = value;
         }
-        default_values(object[key], value);
+        if (typeof object[key] === 'object') {
+            default_values(object[key], value);
+        }
+        
     });
 }
