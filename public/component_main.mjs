4637b0a9644c3913278033e820404152aa001d7e
JaredMathis <jared.mathis@native.consulting>
2023-01-17 17:13:21 -0500

    Tue Jan 17 2023 17:13:21 GMT-0500 (Eastern Standard Time)

--- public/component_main.mjs
+++ public/component_main.mjs
@@ -1,14 +1,12 @@
 import { element_button_width_full } from "./element_button_width_full.mjs";
+import { element_style_font_size_standard } from "./element_style_font_size_standard.mjs";
 
 export function component_main(parent) {
     const button_text = 'New';
     let e = element_button_width_full(parent, button_text);
-    element_font_size_standard(e);
+    element_style_font_size_standard(e);
     e.style.borderRadius = `2vh`
 }
 
 
-function element_font_size_standard(e) {
-    e.style.fontSize = `4vh`;
-}
 
---
+++ public/element_style_font_size_standard.mjs
@@ -0,0 +1,3 @@
+export function element_style_font_size_standard(e) {
+    e.style.fontSize = `4vh`;
+}
