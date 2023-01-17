1460e3807a6bf76c5f9090fe3ecf3731ca790e2d
JaredMathis <jared.mathis@native.consulting>
2023-01-17 18:32:11 -0500

    Tue Jan 17 2023 18:32:11 GMT-0500 (Eastern Standard Time)

--- public/component_main.mjs
+++ public/component_main.mjs
@@ -1,3 +1,4 @@
+import { button_back } from "./button_back.mjs";
 import { component_settings } from "./component_settings.mjs";
 import { element_button_standard } from "./element_button_standard.mjs";
 import { noop } from "./noop.mjs";
@@ -5,11 +6,15 @@ import { view_set } from "./view_set.mjs";
 
 export function component_main(parent, view) {
     element_button_standard(
-        parent, 'New', noop);
+        parent, 'New', view_set(view, component_new));
     element_button_standard(
         parent, 'Open', noop);
     element_button_standard(
         parent, 'Settings', view_set(view, component_settings));
+}
+
+function component_new(parent, view) {
+    button_back(parent, view);
 }
 
 
