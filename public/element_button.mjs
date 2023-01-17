d3e2983196295dbba6ca6c0d9ff7f9ff82c72659
JaredMathis <jared.mathis@native.consulting>
2023-01-17 17:27:36 -0500

    Tue Jan 17 2023 17:27:36 GMT-0500 (Eastern Standard Time)

--- public/element_button.mjs
+++ public/element_button.mjs
@@ -1,6 +1,8 @@
 import { element } from "./element.mjs";
+import { element_on_click } from "./element_on_click.mjs";
 
 export function element_button(parent, button_text, on_click) {
     let b = element(parent, 'button', button_text);
+    element_on_click(parent, on_click);
     return b;
 }
