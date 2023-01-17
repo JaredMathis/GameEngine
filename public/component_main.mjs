236836ec6799a6f5a012aee06015fd6502aada7e
JaredMathis <jared.mathis@native.consulting>
2023-01-17 17:18:48 -0500

    Tue Jan 17 2023 17:18:48 GMT-0500 (Eastern Standard Time)

--- public/component_main.mjs
+++ public/component_main.mjs
@@ -1,12 +1,11 @@
 import { element_button_standard } from "./element_button_standard.mjs";
+import { element_style_background_color_standard } from "./element_style_background_color_standard.mjs";
 
 export function component_main(parent) {
+    element_style_background_color_standard(parent);
     element_button_standard(parent, 'New');
     element_button_standard(parent, 'Open');
 }
-
-
-
 
 
 
---
+++ public/element_style_background_color_standard.mjs
@@ -0,0 +1,3 @@
+export function element_style_background_color_standard(parent) {
+    parent.style.background = 'black';
+}
