abdb9da19c7e5b923539625f3035b000a4ddf8c6
JaredMathis <jared.mathis@native.consulting>
2023-01-17 18:41:25 -0500

    Tue Jan 17 2023 18:41:25 GMT-0500 (Eastern Standard Time)

--- public/document_body_style_set.mjs
+++ public/document_body_style_set.mjs
@@ -4,4 +4,5 @@ import { element_style_standard } from "./element_style_standard.mjs";
 export function document_body_style_set() {
     element_style_background_color_standard(document.body);
     element_style_standard(document.body);
+    document.body.style.width = 'inherit';
 }
