27afef3fb7f1150766c3626867ab0c2f73a34321
JaredMathis <jared.mathis@native.consulting>
2023-01-17 17:30:10 -0500

    Tue Jan 17 2023 17:30:10 GMT-0500 (Eastern Standard Time)

--- public/component_main.mjs
+++ public/component_main.mjs
@@ -7,9 +7,15 @@ export function component_main(parent) {
     element_button_standard(parent, 'New', noop);
     element_button_standard(parent, 'Open', noop);
     element_button_standard(parent, 'Settings', () => {
-
     });
 }
 
+function view_create(parent) {
+    let child = element_div(parent);
+}
 
+
+function element_div(parent) {
+    return element(parent, 'div');
+}
 
