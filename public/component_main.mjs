75885fec1e52df5de324a92c71a16e0b8c126711
JaredMathis <jared.mathis@native.consulting>
2023-01-17 17:14:12 -0500

    Tue Jan 17 2023 17:14:12 GMT-0500 (Eastern Standard Time)

--- public/component_main.mjs
+++ public/component_main.mjs
@@ -1,16 +1,18 @@
 import { element_button_width_full } from "./element_button_width_full.mjs";
+import { element_style_border_rounded } from "./element_style_border_rounded.mjs";
 import { element_style_font_size_standard } from "./element_style_font_size_standard.mjs";
 
 export function component_main(parent) {
     const button_text = 'New';
     let e = element_button_width_full(parent, button_text);
-    element_style_font_size_standard(e);
-    element_style_border_rounded(e);
+    element_style_standard(e);
 }
 
 
 
-function element_style_border_rounded(e) {
-    e.style.borderRadius = `2vh`;
+
+function element_style_standard(e) {
+    element_style_font_size_standard(e);
+    element_style_border_rounded(e);
 }
 
---
+++ public/element_style_border_rounded.mjs
@@ -0,0 +1,3 @@
+export function element_style_border_rounded(e) {
+    e.style.borderRadius = `2vh`;
+}
