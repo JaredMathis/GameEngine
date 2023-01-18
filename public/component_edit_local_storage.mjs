7ed8a2fe8c205409992043767a0411dddabc3602
JaredMathis <jared.mathis@native.consulting>
2023-01-18 14:21:06 -0500

    Wed Jan 18 2023 14:21:06 GMT-0500 (Eastern Standard Time)

--- public/component_edit_local_storage.mjs
+++ public/component_edit_local_storage.mjs
@@ -1,7 +1,7 @@
 import { local_storage_object_get } from "./local_storage_object_get.mjs";
 import { local_storage_object_set } from "./local_storage_object_set.mjs";
 import { property_get } from "./property_get.mjs";
-import { component_edit } from "./component_edit";
+import { component_edit } from "./component_edit.mjs";
 
 export function component_edit_local_storage(fields, prefix, id_initial) {
     return component_edit(fields, initial_values_get, on_save, !!id_initial);
