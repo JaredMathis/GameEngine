f11cf6a877aa9389a5687e9b6f0fc687e05ad6cc
JaredMathis <jared.mathis@native.consulting>
2023-01-17 20:04:11 -0500

    Tue Jan 17 2023 20:04:11 GMT-0500 (Eastern Standard Time)

--- public/component_open.mjs
+++ public/component_open.mjs
@@ -3,9 +3,11 @@ import { component_empty } from "./component_empty.mjs";
 import { games_get } from "./games_get.mjs";
 import { game_prefix } from "./game_prefix.mjs";
 
-export function component_open(parent, view) {
-    let choices = games_get()
-        .map(k => k.substring(game_prefix().length));
-    component_choice_multiple(
-        parent, choices, 'selected_game', view, component_empty);
+export function component_open(prefix) {
+    return function (parent, view) {
+        let choices = games_get()
+            .map(k => k.substring(prefix.length));
+        component_choice_multiple(
+            parent, choices, 'selected_' + prefix, view, component_empty);
+    }
 }
