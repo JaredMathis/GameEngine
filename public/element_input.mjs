a0fa939e7f7bad0e76ce5aa7ebdf955a0973f5de
JaredMathis <jared.mathis@native.consulting>
2023-01-18 14:49:54 -0500

    Wed Jan 18 2023 14:49:54 GMT-0500 (Eastern Standard Time)

--- public/element_input.mjs
+++ public/element_input.mjs
@@ -1,7 +1,10 @@
 import { element } from "./element.mjs";
+import { element_html_inner } from "./element_html_inner.mjs";
 import { element_style_form } from "./element_style_form.mjs";
 
 export function element_input(parent, placeholder) {
+    let label = element(parent, 'label');
+    element_html_inner(label, placeholder);
     let input = element(parent, 'input');
     input.placeholder = placeholder;
     element_style_form(input);
