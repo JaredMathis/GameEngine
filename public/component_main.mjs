f41204e79c62980c247daba406540d1c73850495
JaredMathis <jared.mathis@native.consulting>
2023-01-17 19:05:51 -0500

    Tue Jan 17 2023 19:05:51 GMT-0500 (Eastern Standard Time)

--- public/component_main.mjs
+++ public/component_main.mjs
@@ -1,16 +1,23 @@
 import { component_new } from "./component_new.mjs";
 import { component_settings } from "./component_settings.mjs";
 import { element_button_standard } from "./element_button_standard.mjs";
-import { noop } from "./noop.mjs";
+import { game_prefix } from "./game_prefix.mjs";
 import { view_set } from "./view_set.mjs";
 
 export function component_main(parent, view) {
     element_button_standard(
         parent, 'New', view_set(view, component_new));
     element_button_standard(
-        parent, 'Open', noop);
+        parent, 'Open', view_set(view, component_open));
     element_button_standard(
         parent, 'Settings', view_set(view, component_settings));
+}
+
+function component_open(parent, view) {
+    let games = Object.keys(localStorage).filter(k => k.startsWith(game_prefix()));
+    for (let game of games) {
+        console.log(game)
+    }
 }
 
 
