ce0cbf58d0cb99cb917a6820a8eaf4bdfdb64bdc
JaredMathis <jared.mathis@native.consulting>
2023-01-17 21:46:07 -0500

    Tue Jan 17 2023 21:46:06 GMT-0500 (Eastern Standard Time)

--- public/element_on_click.mjs
+++ public/element_on_click.mjs
@@ -1,5 +1,9 @@
 export function element_on_click(button, on_click) {
     let event = 'click'
+    element_on(button, event, on_click);
+}
+
+function element_on(button, event, on_click) {
     button.addEventListener(event, async () => {
         await on_click();
     });
