bce6bf5a814b0824a192ecdce869f128c9af4dc0
JaredMathis <jared.mathis@native.consulting>
2023-01-18 15:25:43 -0500

    Wed Jan 18 2023 15:25:43 GMT-0500 (Eastern Standard Time)

--- public/component_edit_local_storage_property.mjs
+++ public/component_edit_local_storage_property.mjs
@@ -1,9 +1,10 @@
 import { component_edit } from "./component_edit.mjs";
 import { local_storage_object_prefixed_save } from "./local_storage_object_prefixed_save.mjs";
 
-export function component_edit_local_storage_property(parent, prefix, stored, property_name, id) {
+export function component_edit_local_storage_property(
+    field_infos, prefix, stored, property_name, id) {
     return component_edit(
-        parent,
+        field_infos,
         () => stored[property_name][id] || { name: id },
         (value_new) => {
             stored[property_name][id] = value_new;
--- public/component_game.mjs
+++ public/component_game.mjs
@@ -1,6 +1,5 @@
-import { game_prefix } from "./game_prefix.mjs";
 import { component_new_open } from "./component_new_open.mjs";
-import { component_empty } from "./component_empty.mjs";
+import { component_button_view } from "./component_button_view.mjs";
 
 export function component_game(parent, view) {
     component_new_open(
@@ -9,7 +8,17 @@ export function component_game(parent, view) {
         game_prefix(), [
             { id: 'name', name: 'Name of Game', type: 'string', },
         ],
-        component_empty
+        (parent, view, stored) => {
+            let field_infos = [
+                { id:'name', name: 'Name', type: 'string' },
+            ]
+            component_button_view(
+                parent, 
+                view, 
+                'On Tile Choose', 
+                component_edit_local_storage_property(
+                    field_infos, game_prefix(), stored, 'on_tile_choose', '1'))
+        }
         );
 }
 
