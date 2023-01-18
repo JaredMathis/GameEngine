1313795879d7c6b5bfd76588fd6372fcc6a7e863
JaredMathis <jared.mathis@native.consulting>
2023-01-17 19:35:56 -0500

    Tue Jan 17 2023 19:35:56 GMT-0500 (Eastern Standard Time)

--- public/component_settings.mjs
+++ public/component_settings.mjs
@@ -1,11 +1,10 @@
-import { view_set_get } from "./view_set_get.mjs";
-import { element_button_standard } from "./element_button_standard.mjs";
 import { button_back } from "./button_back.mjs";
 import { component_settings_font_size } from "./component_settings_font_size.mjs";
+import { component_button_view } from "./component_button_view.mjs";
 
 export function component_settings(parent, view) {
-    element_button_standard(
-        parent, 'Font Size', view_set_get(view, component_settings_font_size));
+    component_button_view(
+        parent,  view, 'Font Size', component_settings_font_size);
     button_back(parent, view);
 }
 
