492302cc3c655e2dffe9d4839107c486fabfd1f9
JaredMathis <jared.mathis@native.consulting>
2023-01-17 20:10:07 -0500

    Tue Jan 17 2023 20:10:07 GMT-0500 (Eastern Standard Time)

--- public/component_main.mjs
+++ public/component_main.mjs
@@ -1,6 +1,8 @@
 import { component_button_view } from "./component_button_view.mjs";
 import { component_game } from "./component_game.mjs";
+import { component_new_open } from "./component_new_open.mjs";
 import { component_settings } from "./component_settings.mjs";
+import { tiles_prefix } from "./tiles_prefix.mjs";
 
 export function component_main(parent, view) {
     component_button_view(
@@ -12,6 +14,13 @@ export function component_main(parent, view) {
 }
 
 function component_tiles(parent, view) {
-    
+    component_new_open(
+        parent, 
+        view, 
+        tiles_prefix(), [
+            { id: 'name', name: 'Name of Tile Set', type: 'string', },
+        ]
+        );
 }
+
 
