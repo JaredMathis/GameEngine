f2ce2fbc7cf3876e9a26ec5e1563e4d7c92d4f0c
JaredMathis <jared.mathis@native.consulting>
2023-01-17 21:41:17 -0500

    Tue Jan 17 2023 21:41:17 GMT-0500 (Eastern Standard Time)

--- public/component_empty.mjs
+++ public/component_empty.mjs
@@ -1,5 +1,4 @@
-import { button_back } from "./button_back.mjs";
 
 export function component_empty(parent, view) {
-    button_back(parent, view);
 }
+
---
+++ public/component_empty_with_back.mjs
@@ -0,0 +1,5 @@
+import { button_back } from "./button_back.mjs";
+
+export function component_empty_with_back(parent, view) {
+    button_back(parent, view);
+}
