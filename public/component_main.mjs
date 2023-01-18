2c5e504ddf0afe417272c2ce7912965a661d4085
JaredMathis <jared.mathis@native.consulting>
2023-01-17 19:18:43 -0500

    Tue Jan 17 2023 19:18:43 GMT-0500 (Eastern Standard Time)

--- public/component_main.mjs
+++ public/component_main.mjs
@@ -17,11 +17,11 @@ export function component_main(parent, view) {
 
 function component_open(parent, view) {
     button_back(parent, view);
+    const prefix = game_prefix();
     let games = Object
         .keys(localStorage)
-        .filter(k => k.startsWith(game_prefix()));
-
-    let choices = games.map(k => k.substring(game_prefix().length));
+        .filter(k => k.startsWith(prefix));
+    let choices = games.map(k => k.substring(prefix.length));
     let choose = element_select(parent, choices);
     choose.focus();
 }
