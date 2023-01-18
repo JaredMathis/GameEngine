2662136a8606289097937d60a1aac31cc7eb538b
JaredMathis <jared.mathis@native.consulting>
2023-01-18 14:33:24 -0500

    Wed Jan 18 2023 14:33:24 GMT-0500 (Eastern Standard Time)

--- public/component_tiles.mjs
+++ public/component_tiles.mjs
@@ -4,6 +4,7 @@ import { component_new_open } from "./component_new_open.mjs";
 import { element_img } from "./element_img.mjs";
 import { element_on_click } from "./element_on_click.mjs";
 import { image_on_load_hidden } from "./image_on_load_hidden.mjs";
+import { local_storage_object_set } from "./local_storage_object_set.mjs";
 import { range } from "./range.mjs";
 import { tiles_prefix } from "./tiles_prefix.mjs";
 import { view_set_get } from "./view_set_get.mjs";
@@ -32,6 +33,9 @@ export function component_tiles(parent, view) {
         ],
         async (parent, view, stored) => {
             let {size, url} = stored;
+            if (!stored.tiles) {
+                stored.tiles = {};
+            }
             let img = await image_on_load_hidden(parent, url);
             let tiles_y = Math.floor(img.height / size);
             let tiles_x = Math.floor(img.width / size);
@@ -57,14 +61,16 @@ export function component_tiles(parent, view) {
                     img.style['object-fit'] = 'none';
                     img.style['object-position'] = `-${x * size}px -${y * size}px`;
                     
+                    let id = y + '_' + x;
+
                     element_on_click(
                         img, 
                         view_set_get(
                             view, 
-                            component_edit_local_storage(
-                                img_fields, 
-                                tiles_prefix() + stored.name + "_",
-                                y + '_' + x)
+                            component_edit(
+                                img_fields,
+                                () => stored.tiles[id] || { id },
+                                () => local_storage_object_set()
                             )
                         )
                 }
