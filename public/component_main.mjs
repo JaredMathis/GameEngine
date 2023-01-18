3e9b7aad2e4cccb6c93d0a940e865ced4f634040
JaredMathis <jared.mathis@native.consulting>
2023-01-17 19:43:06 -0500

    Tue Jan 17 2023 19:43:06 GMT-0500 (Eastern Standard Time)

--- public/component_main.mjs
+++ public/component_main.mjs
@@ -1,3 +1,4 @@
+import { button_back } from "./button_back.mjs";
 import { component_button_view } from "./component_button_view.mjs";
 import { component_new } from "./component_new.mjs";
 import { component_open } from "./component_open.mjs";
@@ -6,17 +7,21 @@ import { games_get } from "./games_get.mjs";
 import { list_empty_not } from "./list_empty_not.mjs";
 
 export function component_main(parent, view) {
-    component_button_view(parent, view, 'New', component_new);
-
-    let games = games_get();
-    if (list_empty_not(games)) {
-        component_button_view(
-            parent, view, 'Open', component_open);
-    }
-
+    component_button_view(
+        parent, view, 'Game', component_game);
     component_button_view(
         parent, view, 'Settings', component_settings);
 }
 
 
+
+function component_game(parent, view) {
+    component_button_view(parent, view, 'New', component_new);
+    let games = games_get();
+    if (list_empty_not(games)) {
+        component_button_view(
+            parent, view, 'Open', component_open);
+    }
+    button_back(parent, view)
+}
 
