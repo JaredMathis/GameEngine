bb4ba5ab5df43356c182b49e290a34c4069e7061
JaredMathis <jared.mathis@native.consulting>
2023-01-18 15:35:22 -0500

    Wed Jan 18 2023 15:35:22 GMT-0500 (Eastern Standard Time)

--- public/component_tags.mjs
+++ public/component_tags.mjs
@@ -1,6 +1,7 @@
+import { component_new_open } from "./component_new_open.mjs";
 import { tag_prefix } from "./tag_prefix.mjs";
 
-export function component_tags() {
+export function component_tags(parent, view) {
     component_new_open(
         parent,
         view,
