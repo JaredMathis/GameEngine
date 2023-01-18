0e16e459396574ad1567d1ab7205bfa081c62af9
JaredMathis <jared.mathis@native.consulting>
2023-01-18 14:53:00 -0500

    Wed Jan 18 2023 14:53:00 GMT-0500 (Eastern Standard Time)

--- public/element_input.mjs
+++ public/element_input.mjs
@@ -1,10 +1,12 @@
 import { element } from "./element.mjs";
 import { element_html_inner } from "./element_html_inner.mjs";
+import { element_style_font_size_small } from "./element_style_font_size_small.mjs";
 import { element_style_form } from "./element_style_form.mjs";
 
 export function element_input(parent, placeholder) {
     let label = element(parent, 'label');
     element_html_inner(label, placeholder);
+    element_style_font_size_small(label);
     let input = element(parent, 'input');
     input.placeholder = placeholder;
     element_style_form(input);
