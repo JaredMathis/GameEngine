b7b5dc81ec4d7b421ecb2ed42a3a130fb7f7540e
JaredMathis <jared.mathis@native.consulting>
2023-01-17 20:04:15 -0500

    Tue Jan 17 2023 20:04:15 GMT-0500 (Eastern Standard Time)

--- public/component_open.mjs
+++ public/component_open.mjs
@@ -1,7 +1,6 @@
 import { component_choice_multiple } from "./component_choice_multiple.mjs";
 import { component_empty } from "./component_empty.mjs";
 import { games_get } from "./games_get.mjs";
-import { game_prefix } from "./game_prefix.mjs";
 
 export function component_open(prefix) {
     return function (parent, view) {
