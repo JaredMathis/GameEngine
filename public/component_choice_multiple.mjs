fe308923f24338b7e2fa69464278c08dee3ae397
JaredMathis <jared.mathis@native.consulting>
2023-01-18 22:30:31 -0500

    Wed Jan 18 2023 22:30:31 GMT-0500 (Eastern Standard Time)

--- public/component_choice_multiple.mjs
+++ public/component_choice_multiple.mjs
@@ -4,7 +4,8 @@ import { element_select } from "./element_select.mjs";
 import { local_storage_set } from "./local_storage_set.mjs";
 import { view_set } from "./view_set.mjs";
 
-export function component_choice_multiple(parent, choices, local_storage_key, view, next_screen) {
+export function component_choice_multiple(
+    parent, choices, local_storage_key, view, next_screen) {
     button_back(parent, view);
     let choose = element_select(parent, choices);
     element_button_standard(parent, 'Choose', () => {
