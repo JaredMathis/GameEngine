8b082f8dafc27ec79202221d4fda53e9d0919958
JaredMathis <jared.mathis@native.consulting>
2023-01-19 11:09:21 -0500

    Thu Jan 19 2023 11:09:21 GMT-0500 (Eastern Standard Time)

--- public/tag_exists.mjs
+++ public/tag_exists.mjs
@@ -2,6 +2,11 @@ import { game_object_tags_get } from "./game_object_tags_get.mjs";
 import { list_single } from "./list_single.mjs";
 
 export function tag_exists(tags_all, ancestors, game_object, tag) {
+    let matches = tags_all.filter(t => t.name === tag);
+    if (matches.length > 1) {
+        throw new Error('Multiple tags matched ' + tag);
+    }
+
     let tags_for_value = game_object_tags_get(game_object, ancestors);
     if (tags_for_value.length) {
         let final = list_single(tags_for_value);
