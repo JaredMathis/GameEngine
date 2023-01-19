483925ca2e6c97991aa132276c50e43094f3e51e
JaredMathis <jared.mathis@native.consulting>
2023-01-19 05:56:32 -0500

    Thu Jan 19 2023 05:56:32 GMT-0500 (Eastern Standard Time)

--- public/component_game.mjs
+++ public/component_game.mjs
@@ -1,6 +1,5 @@
 import { component_new_open_local_storage } from "./component_new_open_local_storage.mjs";
 import { game_prefix } from "./game_prefix.mjs";
-import { game_on_tile_choose } from "./game_on_tile_choose.mjs";
 
 export function component_game(parent, view) {
     component_new_open_local_storage(
@@ -11,7 +10,12 @@ export function component_game(parent, view) {
             { id: 'name', name: 'Name of Game', type: 'string', },
         ],
         (parent, view, stored) => {
-            game_on_tile_choose(parent, view, stored);
+            component_button_property_child(
+                parent, view, 'On Tile Choose', game_prefix(), stored, 'on_tile_choose', [
+                    { id: 'name', name: 'Name', type: 'string' },
+                    { id: 'requirement', name: 'Requirement', type: 'string' },
+                    { id: 'action', name: 'Action', type: 'string' },
+                ]);
         }
         );
 }
