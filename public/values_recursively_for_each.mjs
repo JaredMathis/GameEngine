598a71601deff49f05a142bd6879657fea24f28f
JaredMathis <jared.mathis@native.consulting>
2023-01-19 10:40:07 -0500

    Thu Jan 19 2023 10:40:07 GMT-0500 (Eastern Standard Time)

--- public/values_recursively_for_each.mjs
+++ public/values_recursively_for_each.mjs
@@ -1,11 +1,16 @@
 import { values_for_each } from "./values_for_each.mjs";
 
-export function values_recursively_for_each(object, for_each, ancestors) {
+export function values_recursively_for_each(object, for_each, ancestors, except) {
     if (!ancestors) {
         ancestors = [];
     }
     ancestors.push(object);
     values_for_each(object, value => {
+        if (except) {
+            if (except(value)) {
+                return;
+            }
+        }
         for_each(value, ancestors);
         if (typeof value === typeof {}) {
             values_recursively_for_each(value, for_each, ancestors);
