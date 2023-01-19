273c1a4aabfe4a924ad6bb593db7697028ba833a
JaredMathis <jared.mathis@native.consulting>
2023-01-19 13:46:24 -0500

    Thu Jan 19 2023 13:46:24 GMT-0500 (Eastern Standard Time)

--- public/game_object_tags_get.mjs
+++ public/game_object_tags_get.mjs
@@ -3,10 +3,12 @@ export function game_object_tags_get(value, ancestors) {
     values.splice(0, 0, value);
     
     let tags_for_value = [];
-    if (value.tags) {
-        let { tags } = value;
-        if (tags) {
-            tags_for_value = tags.split(',');
+    for (let v of values) {
+        if (v.tags) {
+            let { tags } = v;
+            if (tags) {
+                tags_for_value = tags.split(',');
+            }
         }
     }
     return tags_for_value;
