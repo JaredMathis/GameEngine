69cc971211095f7a29467e743cb513a92c4aaa03
JaredMathis <jared.mathis@native.consulting>
2023-01-19 05:56:15 -0500

    Thu Jan 19 2023 05:56:15 GMT-0500 (Eastern Standard Time)

--- public/game_on_tile_choose.mjs
+++ public/game_on_tile_choose.mjs
@@ -2,15 +2,11 @@ import { game_prefix } from "./game_prefix.mjs";
 import { component_button_property_child } from "./component_button_property_child.mjs";
 
 export function game_on_tile_choose(parent, view, stored) {
-    let field_infos = [
-        { id: 'name', name: 'Name', type: 'string' },
-        { id: 'requirement', name: 'Requirement', type: 'string' },
-        { id: 'action', name: 'Action', type: 'string' },
-    ];
-    let title = 'On Tile Choose';
-    let property_name = 'on_tile_choose';
-    let prefix = game_prefix();
     component_button_property_child(
-        parent, view, title, prefix, stored, property_name, field_infos);
+        parent, view, 'On Tile Choose', game_prefix(), stored, 'on_tile_choose', [
+            { id: 'name', name: 'Name', type: 'string' },
+            { id: 'requirement', name: 'Requirement', type: 'string' },
+            { id: 'action', name: 'Action', type: 'string' },
+        ]);
 }
 
