594a4016fb79377738d5cb4f7eb09532804e14a3
JaredMathis <jared.mathis@native.consulting>
2023-01-19 13:56:13 -0500

    Thu Jan 19 2023 13:56:13 GMT-0500 (Eastern Standard Time)

--- public/game_object_except.mjs
+++ public/game_object_except.mjs
@@ -1,5 +1,5 @@
 import { html_element_is } from "./html_element_is.mjs";
 
 export function game_object_except() {
-    return value => html_element_is(value);
+    return value => html_element_is(value) || typeof value === typeof '';
 }
