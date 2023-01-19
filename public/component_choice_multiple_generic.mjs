77a941314cdc2bc0890f8d29bafb750a2c064b29
JaredMathis <jared.mathis@native.consulting>
2023-01-19 05:51:20 -0500

    Thu Jan 19 2023 05:51:20 GMT-0500 (Eastern Standard Time)

--- public/component_choice_multiple_generic.mjs
+++ public/component_choice_multiple_generic.mjs
@@ -2,7 +2,8 @@ import { element_button_standard } from "./element_button_standard.mjs";
 import { element_select } from "./element_select.mjs";
 import { view_set } from "./view_set.mjs";
 
-export function component_choice_multiple_generic(parent, view, choices, on_choose, next_screen) {
+export function component_choice_multiple_generic(
+    parent, view, choices, on_choose, next_screen) {
     let select = element_select(parent, choices);
     element_button_standard(parent, 'Choose', () => {
         on_choose(select.value);
