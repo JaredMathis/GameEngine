28c0266339d0fa6dc9e265e74e1da5c8b56c65d0
JaredMathis <jared.mathis@native.consulting>
2023-01-19 10:47:50 -0500

    Thu Jan 19 2023 10:47:50 GMT-0500 (Eastern Standard Time)

--- public/game_objects_by_tag_get.mjs
+++ public/game_objects_by_tag_get.mjs
@@ -3,14 +3,8 @@ import { values_recursively_for_each } from "./values_recursively_for_each.mjs";
 
 export function game_objects_by_tag_get(game_objects, tags_all, tag) {
     let result = [];
-    values_recursively_for_each(game_objects, value => {
-        let tags_for_value = [];
-        if (value.tags) {
-            let { tags } = value;
-            if (tags) {
-                tags_for_value = tags.split(',');
-            }
-        }
+    values_recursively_for_each(game_objects, (value) => {
+        let tags_for_value = game_object_tags_get(value);
         let final = list_single(tags_for_value);
         if (final === tag) {
             result.push(value);
@@ -18,3 +12,14 @@ export function game_objects_by_tag_get(game_objects, tags_all, tag) {
     }, undefined, value => value instanceof HTMLElement);
     return result;
 }
+function game_object_tags_get(value) {
+    let tags_for_value = [];
+    if (value.tags) {
+        let { tags } = value;
+        if (tags) {
+            tags_for_value = tags.split(',');
+        }
+    }
+    return tags_for_value;
+}
+
