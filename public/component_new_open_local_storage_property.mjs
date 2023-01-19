65f255180db9d62e56b183731439ba8e71557c26
JaredMathis <jared.mathis@native.consulting>
2023-01-19 05:24:09 -0500

    Thu Jan 19 2023 05:24:09 GMT-0500 (Eastern Standard Time)

--- public/component_new_open_local_storage_property.mjs
+++ public/component_new_open_local_storage_property.mjs
@@ -3,6 +3,7 @@ import { local_storage_key_selected_get } from "./local_storage_key_selected_get
 import { local_storage_object_get } from "./local_storage_object_get.mjs";
 import { component_new_open_generic } from "./component_new_open_generic.mjs";
 import { component_open_generic } from "./component_open_generic.mjs";
+import { property_get } from "./property_get.mjs";
 
 export function component_new_open_local_storage_property(
     parent, view, prefix, stored, property_name, fields, component_on_open) {
@@ -12,7 +13,7 @@ export function component_new_open_local_storage_property(
         return component_edit_local_storage_property(fields, prefix, stored, property_name);
     };
     let component_edit_get = (entity) => component_edit_local_storage_property(
-        fields, prefix, stored, property_name, entity.name, true);
+        fields, prefix, stored, property_name, property_get(entity, 'name'), true);
     let entity;
     let entity_get = () => entity
     let component_open_get = (component_opened) => { 
