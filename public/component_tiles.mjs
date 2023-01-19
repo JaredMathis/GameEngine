d2d63dd3e08f4d316a3e3481ec3b76df01a065d5
JaredMathis <jared.mathis@native.consulting>
2023-01-19 15:09:21 -0500

    Thu Jan 19 2023 15:09:21 GMT-0500 (Eastern Standard Time)

--- public/component_tiles.mjs
+++ public/component_tiles.mjs
@@ -1,6 +1,7 @@
 import { component_edit_local_storage_property } from "./component_edit_local_storage_property.mjs";
 import { component_new_open_local_storage } from "./component_new_open_local_storage.mjs";
 import { element_on_click } from "./element_on_click.mjs";
+import { img_container } from "./img_container.mjs";
 import { img_from_tile_set } from "./img_from_tile_set.mjs";
 import { img_to_count } from "./img_to_count.mjs";
 import { range } from "./range.mjs";
@@ -48,7 +49,8 @@ export function component_tiles(parent, view) {
 
             for (let y of range(y_count)) {
                 for (let x of range(x_count)) {
-                    let img = img_from_tile_set(parent, root, x, y);
+                    let container = img_container(parent, root.size);
+                    let img = img_from_tile_set(container, root, x, y);
                     
                     let id = y + '_' + x;
 
