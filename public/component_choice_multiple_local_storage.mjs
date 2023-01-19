78a5cc33db6c724d4fcb2f22bcd64d6befdf824c
JaredMathis <jared.mathis@native.consulting>
2023-01-19 04:47:51 -0500

    Thu Jan 19 2023 04:47:51 GMT-0500 (Eastern Standard Time)

--- public/component_choice_multiple_local_storage.mjs
+++ public/component_choice_multiple_local_storage.mjs
@@ -4,11 +4,11 @@ import { local_storage_set } from "./local_storage_set.mjs";
 export function component_choice_multiple_local_storage(
     parent, choices, local_storage_key, view, next_screen) {
 
-    let choose = component_choice_multiple_generic(
+    let select = component_choice_multiple_generic(
         parent, view, choices, on_choose, next_screen);
 
     function on_choose() {
-        local_storage_set(local_storage_key, current => choose.value);
+        local_storage_set(local_storage_key, current => select.value);
     }
 }
 
