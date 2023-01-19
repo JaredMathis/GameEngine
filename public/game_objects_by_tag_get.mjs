f5adb652958cc557e0f0dfdb2cc75a38056e1301
JaredMathis <jared.mathis@native.consulting>
2023-01-19 13:55:54 -0500

    Thu Jan 19 2023 13:55:54 GMT-0500 (Eastern Standard Time)

--- public/game_objects_by_tag_get.mjs
+++ public/game_objects_by_tag_get.mjs
@@ -4,13 +4,16 @@ import { game_object_except } from "./game_object_except.mjs";
 
 export function game_objects_by_tag_get(game_objects, tags_all, tag) {
     let result = [];
-    values_recursively_for_each(game_objects, (value, ancestors) => {
-        if (tag_exists(tags_all, ancestors, value, tag)) {
-            result.push(value);
-        }
-    }, 
-    undefined, 
-    game_object_except());
+    values_recursively_for_each(
+        game_objects, 
+        (value, ancestors) => {
+            if (tag_exists(tags_all, ancestors, value, tag)) {
+                result.push(value);
+            }
+        }, 
+        undefined, 
+        game_object_except()
+        );
     return result;
 }
 
